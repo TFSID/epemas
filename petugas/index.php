4ee6363e1d59f469049d7d82f9603423f151cbc3
Sanji Winaya <sanzlvr654@gmail.com>
2023-03-10 00:03:00 +0700

    update petugas

--- petugas/index.php
+++ petugas/index.php
@@ -1,3 +1,12 @@
+<?php
+session_start();
+
+// if ($_SESSION['level'] == "level") {
+//     header("location:../login.php?info=login");
+// }
+
+?>
+
 <!DOCTYPE html>
 <html>
 
@@ -59,17 +68,11 @@
             }
         }
     </style>
-    <script defer="defer" src="main.js"></script>
-    <link href="../style.css" rel="stylesheet">
+    <script defer="defer" src="../assets/js/main.js"></script>
+    <link href="../assets/css/style.css" rel="stylesheet">
 </head>
 
 <body class="app">
-
-
-
-
-
-
     <div id="loader">
         <div class="spinner"></div>
     </div>
@@ -101,7 +104,7 @@
                                         </div>
                                     </div>
                                     <div class="peer peer-greed">
-                                        <h5 class="lh-1 mB-0 logo-text">Adminator</h5>
+                                        <h5 class="lh-1 mB-0 logo-text">Petugas</h5>
                                     </div>
                                 </div>
                             </a>
@@ -126,46 +129,7 @@
                             <span class="title">Dashboard</span>
                         </a>
                     </li>
-                    <li class="nav-item">
-                        <a class="sidebar-link" href="email.html">
-                            <span class="icon-holder">
-                                <i class="c-brown-500 ti-email"></i>
-                            </span>
-                            <span class="title">Email</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="sidebar-link" href="compose.html">
-                            <span class="icon-holder">
-                                <i class="c-blue-500 ti-share"></i>
-                            </span>
-                            <span class="title">Compose</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="sidebar-link" href="calendar.html">
-                            <span class="icon-holder">
-                                <i class="c-deep-orange-500 ti-calendar"></i>
-                            </span>
-                            <span class="title">Calendar</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="sidebar-link" href="chat.html">
-                            <span class="icon-holder">
-                                <i class="c-deep-purple-500 ti-comment-alt"></i>
-                            </span>
-                            <span class="title">Chat</span>
-                        </a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="sidebar-link" href="charts.html">
-                            <span class="icon-holder">
-                                <i class="c-indigo-500 ti-bar-chart"></i>
-                            </span>
-                            <span class="title">Charts</span>
-                        </a>
-                    </li>
+
                     <li class="nav-item">
                         <a class="sidebar-link" href="forms.html">
                             <span class="icon-holder">
@@ -174,14 +138,7 @@
                             <span class="title">Forms</span>
                         </a>
                     </li>
-                    <li class="nav-item dropdown">
-                        <a class="sidebar-link" href="ui.html">
-                            <span class="icon-holder">
-                                <i class="c-pink-500 ti-palette"></i>
-                            </span>
-                            <span class="title">UI Elements</span>
-                        </a>
-                    </li>
+
                     <li class="nav-item dropdown">
                         <a class="dropdown-toggle" href="javascript:void(0);">
                             <span class="icon-holder">
@@ -194,91 +151,10 @@
                         </a>
                         <ul class="dropdown-menu">
                             <li>
-                                <a class="sidebar-link" href="basic-table.html">Basic Table</a>
+                                <a class="sidebar-link" href="data_pengaduan.php">Masyarakat</a>
                             </li>
                             <li>
-                                <a class="sidebar-link" href="datatable.html">Data Table</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-toggle" href="javascript:void(0);">
-                            <span class="icon-holder">
-                                <i class="c-purple-500 ti-map"></i>
-                            </span>
-                            <span class="title">Maps</span>
-                            <span class="arrow">
-                                <i class="ti-angle-right"></i>
-                            </span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a href="google-maps.html">Google Map</a>
-                            </li>
-                            <li>
-                                <a href="vector-maps.html">Vector Map</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-toggle" href="javascript:void(0);">
-                            <span class="icon-holder">
-                                <i class="c-red-500 ti-files"></i>
-                            </span>
-                            <span class="title">Pages</span>
-                            <span class="arrow">
-                                <i class="ti-angle-right"></i>
-                            </span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li>
-                                <a class="sidebar-link" href="blank.html">Blank</a>
-                            </li>
-                            <li>
-                                <a class="sidebar-link" href="404.html">404</a>
-                            </li>
-                            <li>
-                                <a class="sidebar-link" href="500.html">500</a>
-                            </li>
-                            <li>
-                                <a class="sidebar-link" href="signin.html">Sign In</a>
-                            </li>
-                            <li>
-                                <a class="sidebar-link" href="signup.html">Sign Up</a>
-                            </li>
-                        </ul>
-                    </li>
-                    <li class="nav-item dropdown">
-                        <a class="dropdown-toggle" href="javascript:void(0);">
-                            <span class="icon-holder">
-                                <i class="c-teal-500 ti-view-list-alt"></i>
-                            </span>
-                            <span class="title">Multiple Levels</span>
-                            <span class="arrow">
-                                <i class="ti-angle-right"></i>
-                            </span>
-                        </a>
-                        <ul class="dropdown-menu">
-                            <li class="nav-item dropdown">
-                                <a href="javascript:void(0);">
-                                    <span>Menu Item</span>
-                                </a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a href="javascript:void(0);">
-                                    <span>Menu Item</span>
-                                    <span class="arrow">
-                                        <i class="ti-angle-right"></i>
-                                    </span>
-                                </a>
-                                <ul class="dropdown-menu">
-                                    <li>
-                                        <a href="javascript:void(0);">Menu Item</a>
-                                    </li>
-                                    <li>
-                                        <a href="javascript:void(0);">Menu Item</a>
-                                    </li>
-                                </ul>
+                                <a class="sidebar-link" href="datatable.html">Laporan</a>
                             </li>
                         </ul>
                     </li>
@@ -328,7 +204,7 @@
                                                 </div>
                                                 <div class="peer peer-greed">
                                                     <span>
-                                                        <span class="fw-500">Petugas</span>
+                                                        <span class="fw-500">John Doe</span>
                                                         <span class="c-grey-600">liked your <span class="text-dark">post</span>
                                                         </span>
                                                     </span>
@@ -403,7 +279,7 @@
                                                     <div>
                                                         <div class="peers jc-sb fxw-nw mB-5">
                                                             <div class="peer">
-                                                                <p class="fw-500 mB-0">Petugas</p>
+                                                                <p class="fw-500 mB-0">John Doe</p>
                                                             </div>
                                                             <div class="peer">
                                                                 <small class="fsz-xs">5 mins ago</small>
@@ -475,7 +351,7 @@
                                     <img class="w-2r bdrs-50p" src="https://randomuser.me/api/portraits/men/10.jpg" alt="">
                                 </div>
                                 <div class="peer">
-                                    <span class="fsz-sm c-grey-900">Petugas</span>
+                                    <span class="fsz-sm c-grey-900">John Doe</span>
                                 </div>
                             </a>
                             <ul class="dropdown-menu fsz-sm">
